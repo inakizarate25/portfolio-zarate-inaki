362060272ad84073968b155d0018828fcf79fd0d
inakizarate25 <inakizarate25@gmail.com>
2022-08-31 19:32:33 -0300

    bbbb

--- form.php
+++ form.php
@@ -5,19 +5,22 @@ $nombre = $_POST ["nombre"];
 $email = $_POST ["email"];
 $mensaje = $_POST ["textarea"];
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje = "Su e-mail es: " . $mail . ",\r\n";
-$mensaje = "Mensaje: " $_POST ["mensaje"] . ",\r\n";
-$mensaje = "Enviado el " . date("d/m/y" time ()) . ;
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-$para = "inakizarate.dev@gmail.com";
-$asunto = "Este mail fue enviado desde Mi Portfolio";
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Mensaje: " . $_POST['textarea'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
+$para = 'escribeaquitucorreo@hotmail.com';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
 
+mail($para, $asunto, utf8_decode($message), $header);
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
-
-header("location:index.html");
+header("Location:index.html");
+?>
 
 
