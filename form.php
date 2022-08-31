8b076ce377f0b095b683a2e1aa1e1d1b6b364c2c
inakizarate25 <inakizarate25@gmail.com>
2022-08-31 19:13:24 -0300

    php

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$nombre = $_POST ["nombre"];
+$email = $_POST ["email"];
+$mensaje = $_POST ["textarea"];
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Su e-mail es: " . $mail . ",\r\n";
+$mensaje = "Mensaje: " $_POST ["mensaje"] . ",\r\n";
+$mensaje = "Enviado el " . date("d/m/y" time ()) . ;
+
+$para = "inakizarate.dev@gmail.com";
+$asunto = "Este mail fue enviado desde Mi Portfolio";
+
+
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+
+header("location:exito.html");
+
+
